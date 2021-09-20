325d4a67547f090028f6fd5b73cb9f6406883d3e
ruairioliverwv <ruairi.oliver@workvivo.com>
2021-09-20 16:44:40 +0100

    Finish reverting android to v4.0.5

--- react-native.config.js
+++ react-native.config.js
@@ -3,11 +3,7 @@ const path = require('path');
 module.exports = {
   dependency: {
     platforms: {
-      ios: { podspecPath: path.join(__dirname, 'RNKeychain.podspec') },
-      android: {
-        packageImportPath: 'import com.oblador.keychain.KeychainPackage;',
-        packageInstance: 'new KeychainPackage()',
-      },
+      ios: { podspecPath: path.join(__dirname, 'RNKeychain.podspec') }
     },
   },
 };
